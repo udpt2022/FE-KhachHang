2b6bac41e52150f556142b718d863f3fd3e313e7
KhoaMinHi <minhkhoa031099@gmail.com>
2022-06-13 02:13:30 +0700

    add ads banner

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $CSS_PATH = "template/index/indexCSS.php";
 $HEADER_PATH = "public/html/header.phtml";
 $CONTENT_PATH = "./template/testFooterWithContent.phtml";
 $FOOTER_PATH = "public/html/footer.phtml";
+$BANNER_PATH = "public/html/banner.phtml";
 
 require_once("./template/template.phtml");
 ?>
--- template/index/indexCSS.php
+++ template/index/indexCSS.php
@@ -3,3 +3,4 @@ echo "<link href='" . $cssRePath . "public/css/header.css' rel='stylesheet' type
 echo "<link href='" . $cssRePath . "public/css/footer.css' rel='stylesheet' type='text/css' />\n";
 echo "<link href='" . $cssRePath . "public/css/global.css' rel='stylesheet' type='text/css' />\n";
 echo "<link href='" . $cssRePath . "public/css/content.css' rel='stylesheet' type='text/css' />\n";
+echo "<link href='" . $cssRePath . "public/css/banner.css' rel='stylesheet' type='text/css' />\n";
