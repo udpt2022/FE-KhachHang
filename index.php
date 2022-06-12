f2b08a2fb4f06c2509bdb830c24125dfd2f58f30
KhoaMinHi <minhkhoa031099@gmail.com>
2022-06-12 18:48:06 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$cssRePath = "";
+$CSS_PATH = "template/index/indexCSS.php";
+$HEADER_PATH = "public/html/header.phtml";
+$CONTENT_PATH = "./template/testFooterWithContent.phtml";
+$FOOTER_PATH = "public/html/footer.phtml";
+
+require_once("./template/template.phtml");
+?>
---
+++ template/index/indexCSS.php
@@ -0,0 +1,5 @@
+<?php
+echo "<link href='" . $cssRePath . "public/css/header.css' rel='stylesheet' type='text/css' />\n";
+echo "<link href='" . $cssRePath . "public/css/footer.css' rel='stylesheet' type='text/css' />\n";
+echo "<link href='" . $cssRePath . "public/css/global.css' rel='stylesheet' type='text/css' />\n";
+echo "<link href='" . $cssRePath . "public/css/content.css' rel='stylesheet' type='text/css' />\n";
